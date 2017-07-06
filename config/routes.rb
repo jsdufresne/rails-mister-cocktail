634458f21b3d9cf97a6e751cfaf330d0f9fd06fb
Jean-Sebastien Dufresne <jsdufresne@iciela.org>
2017-07-06 13:49:51 -0400

    Controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,30 @@
 Rails.application.routes.draw do
+  get 'doses/show'
+
+  get 'doses/new'
+
+  get 'doses/create'
+
+  get 'doses/edit'
+
+  get 'doses/update'
+
+  get 'doses/destroy'
+
+  get 'cocktails/index'
+
+  get 'cocktails/show'
+
+  get 'cocktails/new'
+
+  get 'cocktails/create'
+
+  get 'cocktails/edit'
+
+  get 'cocktails/update'
+
+  get 'cocktails/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doses
 end
