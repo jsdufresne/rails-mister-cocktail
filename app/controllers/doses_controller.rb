d4b2a924d01a5d027ccdefb72b7e6424b52fb68c
Jean-Sebastien Dufresne <jsdufresne@iciela.org>
2017-07-06 13:59:20 -0400

    Controllers populated

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -3,9 +3,19 @@ class DosesController < ApplicationController
   end
 
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
 
   def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+      if @dose.save
+        redirect_to cocktail_path(@cocktail)
+      else
+        render :new
+      end
   end
 
   def edit
@@ -16,4 +26,13 @@ class DosesController < ApplicationController
 
   def destroy
   end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def dose_params
+     params.require(:dose).permit(:description)
+    end
+
 end
