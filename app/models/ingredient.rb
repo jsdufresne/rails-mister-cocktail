64e6526792a967120efe3094a5b702f2b106a1ed
Jean-Sebastien Dufresne <jsdufresne@iciela.org>
2017-07-06 13:36:16 -0400

    Models raking green

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,13 +1,4 @@
 class Ingredient < ApplicationRecord
   has_many :doses
-  before_destroy :check_for_cocktails
-
-  private
-
-  def check_for_cocktails
-    if cocktails.count > 0
-      errors.add_to_base("cannot delete ingredient while cocktails exist")
-      return false
-    end
-  end
+  validates :name, uniqueness: true, presence: true
 end
